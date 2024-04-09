422f58af03ecffb6f44f31d5e640c84ab2b33f5d
kene10-codes <nwobodokenechukwu2@gmail.com>
2024-04-09 14:29:12 -0700

    added files

--- src/services/routes.js
+++ src/services/routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const userRoute = require('../routes/user')
 const blogRoute = require('../routes/blog')
+const passwordResetRoute = require('../routes/passwordReset')
 const error = require('../middewares/error/error')
 
 module.exports = function (app) {
@@ -8,5 +9,6 @@ module.exports = function (app) {
     app.use(express.json({ limit: '10MB' }))
     app.use('/api/user', userRoute)
     app.use('/api/blog', blogRoute)
+    app.use('/api/password-reset', passwordResetRoute)
     app.use(error)
 }
